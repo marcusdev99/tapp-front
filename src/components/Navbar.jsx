2140ded66cc1a47e074feb540a52a46f9902ae4c
marcusdev99 <marcus.sea13@gmail.com>
2023-05-31 17:23:05 -0600

    Creacion de componente Navbar -Navbar y links para la aplicacion.

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+export const Navbar = () => {
+    return (
+        <>
+            <div>
+                <nav className="navbar navbar-expand-lg bg-body-tertiary">
+                    <div className="container-fluid">
+                        <a className="navbar-brand" href="#">Logo</a>
+                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                            <span className="navbar-toggler-icon"></span>
+                        </button>
+                        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                            <div className="navbar-nav">
+                                <a className="nav-link active" aria-current="page" href="#">Inicio</a>
+                                <a className="nav-link" href="#">Unidades</a>
+                                <a className="nav-link" href="#">Transportistas</a>
+                                <a className="nav-link disabled">Perfil</a>
+                            </div>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+        </>
+    )
+}
